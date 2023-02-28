e56d2643f13b6feef2a7a9bdd5e55e4dd82dd9d1
Peniel DIALUNDAMA <dialundamapeniel@gmail.com>
2023-02-28 12:10:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <h3>My env</h3>
         <div>{JSON.stringify(process.env,null,4)}</div>
 
